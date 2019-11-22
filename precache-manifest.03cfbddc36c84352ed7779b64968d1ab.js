ff1521d8a0787097aeaeb0ea17b702468a9f3a33
Marina Faria <maarina.faria@gmail.com>
2019-11-22 16:55:53 -0300

    Updates

---
+++ precache-manifest.03cfbddc36c84352ed7779b64968d1ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40afa4a791550b89768efcabd3c74883",
+    "url": "/plataforma-comercial/index.html"
+  },
+  {
+    "revision": "fccfdcd1161bdc539104",
+    "url": "/plataforma-comercial/static/css/main.2ac41e96.chunk.css"
+  },
+  {
+    "revision": "79bc686a67c7ee31082f",
+    "url": "/plataforma-comercial/static/js/2.6241f301.chunk.js"
+  },
+  {
+    "revision": "fccfdcd1161bdc539104",
+    "url": "/plataforma-comercial/static/js/main.3b489404.chunk.js"
+  },
+  {
+    "revision": "6864982cbaf190fb7c92",
+    "url": "/plataforma-comercial/static/js/runtime-main.377c7db8.js"
+  },
+  {
+    "revision": "509ef9560c0f9c73b1f6aa4fcffaaebc",
+    "url": "/plataforma-comercial/static/media/logo.509ef956.png"
+  }
+]);
